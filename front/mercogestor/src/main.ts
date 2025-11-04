df2fa94d2948e866aa186688272cea37c28d3402
Guilhermesf27 <d2020011524@unifei.edu.br>
2025-11-03 21:41:13 -0300

    service

--- front/mercogestor/src/main.ts
+++ front/mercogestor/src/main.ts
@@ -6,23 +6,10 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 
 import { provideHttpClient } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
-
-// Se você usa Firebase no app, mantenha estes. Senão, pode remover.
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { provideAuth, getAuth } from '@angular/fire/auth';
-import { env } from './env/enviroment'; // confirme o nome do arquivo (environment vs enviroment)
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),          // <- ESSENCIAL
-    provideAnimations(),
-
-    // Firebase (opcional, só se estiver usando mesmo)
-    provideFirebaseApp(() => initializeApp(env.firebaseConfig)),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
+    provideHttpClient(),
   ],
-}).catch(err => console.error(err));
+}).catch(console.error);
