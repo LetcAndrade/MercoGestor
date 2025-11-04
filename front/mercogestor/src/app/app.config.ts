dfc032666e22730941e93c3c7b23de7a4a1bc2fb
Guilhermesf27 <d2020011524@unifei.edu.br>
2025-11-03 21:32:29 -0300

    service

--- front/mercogestor/src/app/app.config.ts
+++ front/mercogestor/src/app/app.config.ts
@@ -1,19 +1,22 @@
+// src/app/app.config.ts
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideAnimations } from '@angular/platform-browser/animations'; 
 
 import { routes } from './app.routes';
-import { env } from '../env/enviroment'; 
+import { env } from '../env/enviroment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),              
+    provideHttpClient(),
     provideFirebaseApp(() => initializeApp(env.firebaseConfig)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
+    provideAnimations(), 
   ]
 };
